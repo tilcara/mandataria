96ca31e255e637fae12541f16faf7ca72179914e
tilcara <enriquesegovianoandroid@gmail.com>
2019-01-13 03:29:49 -0300

    :cloud: User registration and mail validation :thumbsup:

--- sendmail/forms.py
+++ sendmail/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Mail
+
 
 class MailForm(forms.ModelForm):
 	class Meta:
@@ -17,3 +20,9 @@ class MailForm(forms.ModelForm):
 			'subject': forms.TextInput(attrs={'class':'form-control'}),
 			'message': forms.Textarea(attrs={'class':'form-control'}),
 		}
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
--- sendmail/urls.py
+++ sendmail/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import *
 from django.contrib.auth.decorators import login_required
 
@@ -7,5 +7,7 @@ app_name = 'sendmail'
 urlpatterns = [
     path('', home, name = 'home'),
     path('contact/', login_required(MailContact.as_view()), name = 'contact'),
-    path('services/', services, name = 'services')
+    path('services/', services, name = 'services'),
+    path('signup/', signup, name='signup'),
+    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', activate, name='activate')
 ]				
